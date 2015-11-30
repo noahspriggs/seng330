e7cfa48af9849ed0d15b6faacaab9893bd909589
LukeGuan <Luke@Yihes-MacBook-Air.local>
2015-11-29 18:26:29 -0800

    update sh

--- macmake.sh
+++ macmake.sh
@@ -1,2 +1,2 @@
-g++ src/*.cpp -o bin/macrisk -Iinclude -F/Library/Frameworks -framework SFML -framework sfml-audio -framework sfml-graphics -framework sfml-system -framework sfml-window
+g++ src/*.cpp -o bin/macrisk -std=c++11 -Iinclude -F/Library/Frameworks -framework SFML -framework sfml-audio -framework sfml-graphics -framework sfml-system -framework sfml-window
 
